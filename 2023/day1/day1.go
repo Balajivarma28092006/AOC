3d1488c6c250107b0c854aa22aa9b997eed80410
Balajivarma28092006 <prathibharani1234567890@gmail.com>
2026-07-07 11:45:27 +0530

    completed day1 part2 of 2023

--- 2023/day1/day1.go
+++ 2023/day1/day1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func getData(filename string) ([]string, error) {
@@ -55,4 +56,38 @@ func main() {
 	}
 
 	fmt.Println(ans)
+
+	numMap := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"1":     1, "2": 2, "3": 3, "4": 4, "5": 5,
+		"6": 6, "7": 7, "8": 8, "9": 9,
+	}
+
+	ans = 0
+	for _, str := range values {
+		var firstStr, lastStr string
+		for i := 0; i < len(str); i++ {
+			for word := range numMap {
+				if i+len(word) <= len(str) && str[i:i+len(word)] == word {
+					if len(firstStr) == 0 {
+						firstStr = word
+					}
+					lastStr = word
+				}
+			}
+		}
+		fmt.Println(firstStr, lastStr)
+		number := numMap[firstStr]*10 + numMap[lastStr]
+		ans += number
+	}
+	fmt.Println("Part2 answer is: ", ans)
+	regexp.Match()
 }
